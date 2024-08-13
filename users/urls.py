425db22b6699ee8adc3a04ca59acfe838c3a827e
EdikanSam <c3005644@hallam.shu.ac.uk>
2024-08-13 16:40:25 +0100

    added login/logout

---
+++ users/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path 
+from . import views as views
+from django.conf import settings
+from django.conf.urls.static import static
+
+ 
+
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+
+    path('profile/', views.profile, name = 'profile'), 
+
+
+
+
+
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,29 +1,31 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
-#from django.contrib.auth.decorators import login_required  
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required  
 from django.shortcuts import render
 
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! Now you can login!')
-            return redirect('login') 
+            messages.success(request, f'Account created for {username}!')
+            return redirect('itreporting:home') 
         else:
             messages.warning(request, 'Unable to create account.')
     else:
-        form = UserRegisterForm()
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form, 'title': 'Student Registration'})
 
+@login_required 
 def profile(request):
 
     return render(request, 'users/profile.html', {'title': 'Student Profile'})
+
+
 
 
 
