c256230945b19bff06cb0ab4d31cabba0af608d0
BeckyUwahc3078528 <C3078528@hallam.shu.ac.uk>
2024-08-14 13:44:53 +0100

    modified login,logout

--- registerApp/urls.py
+++ registerApp/urls.py
@@ -25,7 +25,5 @@ urlpatterns = [
     path('', include ('studentApp.urls')),
     path('', include ('users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-
-    
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), 
 ]
