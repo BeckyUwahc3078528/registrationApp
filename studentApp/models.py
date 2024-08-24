c7abe9af371ce9593fb3d437fab03d326d2a9f32
BeckyUwahc3078528 <C3078528@hallam.shu.ac.uk>
2024-08-24 22:03:16 +0100

    competed contact page

--- studentApp/models.py
+++ studentApp/models.py
@@ -3,38 +3,34 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
-# class Course(models.Model):
+# Create your models here.        
+# class Message(models.Model):
 
-#     type = models.CharField(max_length=100, choices = [('Business', 'Business'),('Computing', 'Computing'), ('Engineering', 'Engineering'), ('Medicine', 'Medicine')])
-#     room = models.CharField(max_length=100)
-#     urgent = models.BooleanField(default = False)
-#     details = models.TextField()
-#     date_subimtted = models.DateTimeField(default=timezone.now)
+#     name = models.CharField(max_length=100, choices = [('Business', 'Business'),('Computing', 'Computing'), ('Engineering', 'Engineering'), ('Medicine', 'Medicine')])
+#     emailAddress = models.CharField(max_length=100)
+#     subject = models.TextField()
 #     description = models.TextField()
-#     author = models.ForeignKey(User, related_name = 'issues', on_delete=models.CASCADE)
+#     author = models.ForeignKey(User, related_name = 'enquiries', on_delete=models.CASCADE)
+#     date_subimtted = models.DateTimeField(default=timezone.now)
+
 
 #     def __str__(self):
 
-#         return f'{self.type} Issue in {self.room}'
+#         return f'{self.subject} Enquiry in {self.name}'
+    
+#     def get_absolute_url(self):
 
-        
-class Message(models.Model):
+#         return reverse('messageEnquiry_detail', kwargs = {'pk': self.pk})
 
-    name = models.CharField(max_length=100, choices = [('Business', 'Business'),('Computing', 'Computing'), ('Engineering', 'Engineering'), ('Medicine', 'Medicine')])
-    emailAddress = models.CharField(max_length=100)
+class Inquiry(models.Model):
+
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
     subject = models.TextField()
     description = models.TextField()
-    author = models.ForeignKey(User, related_name = 'enquiries', on_delete=models.CASCADE)
     date_subimtted = models.DateTimeField(default=timezone.now)
 
 
     def __str__(self):
 
-        return f'{self.subject} Enquiry in {self.name}'
-    
-    def get_absolute_url(self):
-
-        return reverse('messageEnquiry_detail', kwargs = {'pk': self.pk})
-
-    
+        return f'{self.subject} from {self.name}'
